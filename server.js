92fbcfb40364805f6bf5d3253caedba65bd3b501
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-12-08 20:52:35 -0300

    feat: add initial server setup with routes and database connection

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ import express from "express";
 import connectDB from "./src/config/database.js";
 import dotenv from "dotenv";
 import membroRoutes from "./src/routes/membroRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
+// Configuração do CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Permita apenas o frontend acessar
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    credentials: true, // Se for necessário enviar cookies ou autenticação
+  })
+);
+
+// Middleware para JSON
 app.use(express.json());
+
+// Rotas e outros middlewares
 app.use("/uploads", express.static("src/uploads"));
 app.use("/membros", membroRoutes);
 
+// Conexão com o banco de dados
 connectDB();
 
 app.listen(process.env.PORT, () => {
