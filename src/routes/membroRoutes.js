16bfa954c40bc4ea97b6b2b2c34a89fd1899af2a
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-12-08 20:54:29 -0300

    feat(router): add basic routes for member management

--- src/routes/membroRoutes.js
+++ src/routes/membroRoutes.js
@@ -4,13 +4,15 @@ import {
   criarMembro,
   aprovarMembro,
   listarMembros,
+  atualizarMembro,
+  loginMembro,
 } from "../controllers/membroController.js";
 import { verificarAdmin } from "../middlewares/auth.js";
-import { atualizarMembro } from "../controllers/membroController.js";
 
 const router = express.Router();
 const upload = multer({ dest: "src/uploads/" });
 
+router.post("/login", loginMembro);
 router.post("/cadastro", upload.single("foto"), criarMembro);
 router.put("/aprovar/:id", verificarAdmin, aprovarMembro);
 router.get("/", listarMembros);
