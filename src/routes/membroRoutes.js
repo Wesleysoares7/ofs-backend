79cc6f24ffd6d1c89b63625e5e8ef90a86507e58
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-12-09 16:32:25 -0300

    feat: route to delete

--- src/routes/membroRoutes.js
+++ src/routes/membroRoutes.js
@@ -6,6 +6,7 @@ import {
   listarMembros,
   atualizarMembro,
   loginMembro,
+  deletarMembro,
 } from "../controllers/membroController.js";
 import { verificarAdmin } from "../middlewares/auth.js";
 
@@ -17,5 +18,6 @@ router.post("/cadastro", upload.single("foto"), criarMembro);
 router.put("/aprovar/:id", verificarAdmin, aprovarMembro);
 router.get("/", listarMembros);
 router.put("/:id", atualizarMembro);
+router.delete("/:id", verificarAdmin, deletarMembro);
 
 export default router;
